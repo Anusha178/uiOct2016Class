95f8937eea64f65cb462d74421742c3e184e1aed
anusha lankela <anusha lankela>
2016-11-22 14:42:57 -0600

    primitive data types

---
+++ javascript/11_22_2016/Anusha/datatypes.js
@@ -0,0 +1,35 @@
+// Primitive data types
+// Defining and declaring STRING data type
+var hotelName='Resto';
+var hotelLocation='Frisco,Texas';
+
+// NUMBER  Primitive data type can be a integer/negative numbers/decimal numbers.
+var noOfRooms=30; // integer number
+var noOfAvailableRooms=21;
+var decimalNumber=0.75; //decimal or floating number
+var negativeNumber=-254; //negative number
+var rating=4;
+
+// BOOLEAN Primitive data type : True/False
+var isOpen=true;
+var isBookingAvailable=false;
+
+// NULL Primitive Data type
+var C = null; // I have initialized variable C to null to have no value.
+
+// UNDEFINED Primitive Data type
+var D = undefined; // variable D is un defined and no memory allocated to this.
+
+
+// printing these values in the browser using console.log();
+console.log(hotelName);
+console.log(hotelLocation);
+console.log(noOfRooms);
+console.log(noOfAvailableRooms);
+console.log(rating);
+console.log(negativeNumber);
+console.log(decimalNumber);
+console.log(isOpen);
+console.log(isBookingAvailable);
+console.log(C);
+console.log(D);
